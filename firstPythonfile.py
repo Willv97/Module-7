289335a5d1d58885849470c8caa2fdf48993e610
Willv97 <willv1357@gmail.com>
2023-06-09 10:46:20 -0400

    first attempt

---
+++ firstPythonfile.py
@@ -0,0 +1,18 @@
+name1 = "Jaylen Brown"
+firstname1 = 'Jaylen'
+age1 = 25
+ageAsAString1 ="26"
+message1 = "We'll be back next year"
+print(message1)
+print(firstname1)
+
+name2 = "Jayson Tatum"
+firstname2 = 'Jayson'
+nickname2 = "JT"
+age2 = 25
+ageAsAString2 ="25"
+message2 = "We'll be back next year"
+print(message2)
+print(firstname2)
+
+typesOfShots = ["lay-ups", "3's", "middy's", "dunks", "ft's", "floaters"]
